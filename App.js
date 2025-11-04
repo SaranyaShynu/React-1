85bb19590b94e273c5d9055962cbd81b533af724
SaranyaShynu <saranyarajendran790@gmail.com>
2025-11-04 11:53:54 +0530

    add

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * <div id="parent">
+ * <div id="child">
+ * <h1>I am h1 tag</h1>
+ * </div>
+ * </div>
+ */
+
+
+ const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{}, "Iam an h1 tag"),
+        React.createElement("h2",{}, "Iam an h2 tag")]
+    )
+ )
+ 
+ console.log(parent)  //object
+        const root=ReactDOM.createRoot(document.getElementById("root"))
+        root.render(parent)
