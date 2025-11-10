c3ea6ccae76d4c5259dfb92a7ac45487cc5ac5e4
SaranyaShynu <saranyarajendran790@gmail.com>
2025-11-10 16:29:32 +0530

    add

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
 import RestaurantCard from "./RestaurantCard"
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import resList from "../utils/mockData"
 
 const Body = () =>{
 
 const [listOfRestaurants,setListOfRestaurants] = useState(resList) //local state variable - super powerful variable
+useEffect(()=>{console.log("useEffect called")
+    fetchData()
+},
+    [])
+    const fetchData = async () =>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=D")
+        const json=await data.json
+        console.log(json)
+        setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
 
+    if(listOfRestaurants.length==0){
+        return<h1>Loading...</h1>
+    }
 //let listOfRestaurants=[]  //normal js variable
 
     return (
