7d1fe4d46dd60a91e4241cfbf044c681b9424a71
SaranyaShynu <saranyarajendran790@gmail.com>
2025-11-17 16:44:46 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,9 +18,9 @@ useEffect(()=>{
 const onlineStatus=useOnlineStatus()
 
     return (
-        <div className="flex justify-between bg-pink-100">
+        <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50">
             <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
+                <img className="w-56" src={LOGO_URL} />
             </div>
             <div className="flex items-center">
                 <ul className="flex p-4 m-4">
