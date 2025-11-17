a50f81125f6c4b44b2cc632c904cc978e704c3aa
SaranyaShynu <saranyarajendran790@gmail.com>
2025-11-17 15:53:36 +0530

    add

--- src/components/Body.js
+++ src/components/Body.js
@@ -34,12 +34,12 @@ useEffect(()=>{
     ) : (
 //let listOfRestaurants=[]  //normal js variable
         <div className="body">
-            <div className="filter">
-                <div className="search">
-                    <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+            <div className="filter flex">
+                <div className="search m-4 p-4">
+                    <input type="text" className="border-solid border-black" value={searchText} onChange={(e)=>{
                         setSearchText(e.target.value)
                     }} />
-                    <button onClick={()=>{
+                    <button className="px-4 py-1 bg bg-green-100 m-4"onClick={()=>{
                         //filter the restaurant card and update the UI
                         //searchText
                         console.log(searchText)
@@ -49,7 +49,7 @@ useEffect(()=>{
                      setFilteredRestaurant(filteredRestaurant)
                      }} > Search</button>
                 </div>
-            <button className="filter-btn" onClick={()=>{
+            <button className="px-4 py-2 bg-gray-180" onClick={()=>{
                const filteredList=listOfRestaurants.filter(
                 (res)=>res.data.avgRating>4)
                setFilteredRestaurant(filteredList)
--- src/components/Header.js
+++ src/components/Header.js
@@ -22,18 +22,18 @@ const onlineStatus=useOnlineStatus()
             <div className="logo-container">
                 <img className="logo" src={LOGO_URL} />
             </div>
-            <div className="nav-items">
+            <div className="flex items-center">
                 <ul className="flex p-4 m-4">
-                    <li>Online Status :{onlineStatus?"🟢" : "🔴"}</li>
-                    <li><Link to="/">Home</Link></li>
-                    <li>
+                    <li className="px-4">Online Status :{onlineStatus?"🟢" : "🔴"}</li>
+                    <li className="px-4"><Link to="/">Home</Link></li>
+                    <li className="px-4">
                         <Link to="/about">About Us</Link>
                         </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/contact">Contact Us</Link></li>
-                         <li>
+                         <li className="px-4">
                         <Link to="/grocery">Grocery</Link></li>
-                    <li>Cart</li>
+                    <li className="px-4">Cart</li>
                     <button className="login" onClick={() => { 
                         btnNameReact=="Login" 
                         ? setBtnNameReact("Logout") 
