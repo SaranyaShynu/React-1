43252c5123562eddd0046f73908b3a0d0d41ba71
SaranyaShynu <saranyarajendran790@gmail.com>
2025-11-27 16:22:12 +0530

    add

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,14 +5,23 @@ const ItemList=({items}) =>{
     return (
     <div>
         
-          {items.map(item=>(
-            <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
-               <img src={CDN_URL + item.card.info.imageId} className="w-14"/>
+          {items.map((item)=>(
+            <div 
+            key={item.card.info.id} 
+            className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
+               <div className="w-9/12">
                 <div className="py-2">
                     <span>{item.card.info.name}</span>
                 <span> - र{item.card.info.price ? item.card.info.price / 100 : items.card.info.defaultPrice}</span>
                 </div>
                 <p className="text-xs">{item.card.info.description}</p>
+            </div>
+            <div className="w=3/12 p-4">
+          <div className="absolute">
+            <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg"> Add +</button>
+            </div>
+              <img src={CDN_URL + item.card.info.imageId} className="w-full"/>
+            </div>
             </div>
             ))}
         
